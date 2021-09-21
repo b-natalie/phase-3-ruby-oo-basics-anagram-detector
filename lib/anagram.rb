9cd69f9b90d59d93e85a30ba49fd8c1d8646f741
Natalie Barba <nbarba2010@gmail.com>
2021-09-21 16:01:31 -0700

    commit tree ad0e46345ee15d4c43a42bb1887ff1a6e4fccce6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(target_word)
+        @target_word = target_word
+    end
+
+    def match(array)
+
+        matches = []
+
+        array.each do |word|
+            if word.downcase.chars.sort == @target_word.downcase.chars.sort
+                matches << word
+            end
+            # matches
+        end
+        matches
+    end
+end
